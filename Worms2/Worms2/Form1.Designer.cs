625edd41bffcf7ce41cd2e7e7cd3947a3bbe2686
hectorBrown <45634793+hectorBrown@users.noreply.github.com>
2018-12-05 17:18:37 +0000

    Add files via upload

---
+++ Worms2/Worms2/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace Worms2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.PB_main = new System.Windows.Forms.PictureBox();
+            this.TIM_animate = new System.Windows.Forms.Timer(this.components);
+            this.PNL_constEditor = new System.Windows.Forms.Panel();
+            this.TB_no = new System.Windows.Forms.TrackBar();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TB_trail = new System.Windows.Forms.TrackBar();
+            this.TB_turn = new System.Windows.Forms.TrackBar();
+            this.TB_zoom = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.PB_main)).BeginInit();
+            this.PNL_constEditor.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.TB_no)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TB_trail)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TB_turn)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TB_zoom)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PB_main
+            // 
+            this.PB_main.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.PB_main.Location = new System.Drawing.Point(0, 74);
+            this.PB_main.Name = "PB_main";
+            this.PB_main.Size = new System.Drawing.Size(1298, 429);
+            this.PB_main.TabIndex = 0;
+            this.PB_main.TabStop = false;
+            this.PB_main.Paint += new System.Windows.Forms.PaintEventHandler(this.PB_main_Paint);
+            // 
+            // TIM_animate
+            // 
+            this.TIM_animate.Enabled = true;
+            this.TIM_animate.Interval = 10;
+            this.TIM_animate.Tick += new System.EventHandler(this.TIM_animate_Tick);
+            // 
+            // PNL_constEditor
+            // 
+            this.PNL_constEditor.Controls.Add(this.TB_no);
+            this.PNL_constEditor.Controls.Add(this.label4);
+            this.PNL_constEditor.Controls.Add(this.label3);
+            this.PNL_constEditor.Controls.Add(this.label2);
+            this.PNL_constEditor.Controls.Add(this.label1);
+            this.PNL_constEditor.Controls.Add(this.TB_trail);
+            this.PNL_constEditor.Controls.Add(this.TB_turn);
+            this.PNL_constEditor.Controls.Add(this.TB_zoom);
+            this.PNL_constEditor.Dock = System.Windows.Forms.DockStyle.Top;
+            this.PNL_constEditor.Location = new System.Drawing.Point(0, 0);
+            this.PNL_constEditor.Name = "PNL_constEditor";
+            this.PNL_constEditor.Size = new System.Drawing.Size(1298, 68);
+            this.PNL_constEditor.TabIndex = 1;
+            // 
+            // TB_no
+            // 
+            this.TB_no.Location = new System.Drawing.Point(956, 12);
+            this.TB_no.Maximum = 1000;
+            this.TB_no.Name = "TB_no";
+            this.TB_no.Size = new System.Drawing.Size(330, 45);
+            this.TB_no.TabIndex = 7;
+            this.TB_no.Scroll += new System.EventHandler(this.TB_no_Scroll);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(906, 22);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Number";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(516, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Trail Length";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(162, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Erraticism";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Zoom";
+            // 
+            // TB_trail
+            // 
+            this.TB_trail.Location = new System.Drawing.Point(585, 12);
+            this.TB_trail.Maximum = 200;
+            this.TB_trail.Minimum = 1;
+            this.TB_trail.Name = "TB_trail";
+            this.TB_trail.Size = new System.Drawing.Size(315, 45);
+            this.TB_trail.TabIndex = 2;
+            this.TB_trail.Value = 1;
+            this.TB_trail.Scroll += new System.EventHandler(this.TB_trail_Scroll);
+            // 
+            // TB_turn
+            // 
+            this.TB_turn.Location = new System.Drawing.Point(220, 12);
+            this.TB_turn.Maximum = 90;
+            this.TB_turn.Name = "TB_turn";
+            this.TB_turn.Size = new System.Drawing.Size(290, 45);
+            this.TB_turn.TabIndex = 1;
+            this.TB_turn.Scroll += new System.EventHandler(this.TB_turn_Scroll);
+            // 
+            // TB_zoom
+            // 
+            this.TB_zoom.Location = new System.Drawing.Point(52, 12);
+            this.TB_zoom.Minimum = 2;
+            this.TB_zoom.Name = "TB_zoom";
+            this.TB_zoom.Size = new System.Drawing.Size(104, 45);
+            this.TB_zoom.TabIndex = 0;
+            this.TB_zoom.Value = 2;
+            this.TB_zoom.Scroll += new System.EventHandler(this.TB_zoom_Scroll);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1298, 503);
+            this.Controls.Add(this.PNL_constEditor);
+            this.Controls.Add(this.PB_main);
+            this.Name = "Form1";
+            this.Text = "Worms";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResizeEnd += new System.EventHandler(this.Form1_ResizeEnd);
+            ((System.ComponentModel.ISupportInitialize)(this.PB_main)).EndInit();
+            this.PNL_constEditor.ResumeLayout(false);
+            this.PNL_constEditor.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.TB_no)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TB_trail)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TB_turn)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TB_zoom)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox PB_main;
+        private System.Windows.Forms.Timer TIM_animate;
+        private System.Windows.Forms.Panel PNL_constEditor;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar TB_trail;
+        private System.Windows.Forms.TrackBar TB_turn;
+        private System.Windows.Forms.TrackBar TB_zoom;
+        private System.Windows.Forms.TrackBar TB_no;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Worms2/Worms2/Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Worms2
+{
+    public partial class Form1 : Form
+    {
+        //the boi
+        Boi[] bois;
+        int BOISIZE = 4, TURNSPEED = 15, MAG = 4, TRAILLENGTH = 100, NUMBER = 25;
+        readonly double RADIAN = 0.0174533;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void PB_main_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            //draw boi
+            foreach (Boi boi in bois)
+            {
+                g.FillRectangle(Brushes.Blue, boi.boi);
+                int counter = 255;
+                foreach (Rectangle trailRect in boi.trail)
+                {
+                    if (!(trailRect == null))
+                    {
+                        g.FillRectangle(new SolidBrush(Color.FromArgb(counter, counter, 255)), trailRect);
+                    }
+                    counter -= 255 / TRAILLENGTH;
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            TB_zoom.Value = BOISIZE;
+            TB_turn.Value = TURNSPEED;
+            TB_trail.Value = TRAILLENGTH;
+            TB_no.Value = NUMBER;
+            Init();
+        }
+
+        private void Init()
+        {
+            PB_main.BackColor = Color.White;
+
+            PB_main.Height = Height - PNL_constEditor.Height - 50;
+
+            bois = new Boi[NUMBER];
+            //to position boi
+            Random rng = new Random();
+            for (int i = 0; i <= bois.Length - 1; i++)
+            {
+                Point start = new Point(rng.Next(0, PB_main.Width - BOISIZE + 1), rng.Next(0, PB_main.Height - BOISIZE + 1));
+                bois[i] = new Boi(start.X, start.Y, new Size(BOISIZE, BOISIZE), TRAILLENGTH, rng.Next(0, 360));
+            }
+        }
+
+        private void TIM_animate_Tick(object sender, EventArgs e)
+        {
+            Random rng = new Random();
+            foreach (Boi boi in bois)
+            {
+                if (rng.Next(0, 2) == 0)
+                {
+                    boi.angle += TURNSPEED;
+                }
+                else
+                {
+                    boi.angle -= TURNSPEED;
+                }
+                Point speed = GetXY(boi.angle, MAG);
+                boi.ShiftBack();
+                boi.boi.X += speed.X; boi.boi.Y += speed.Y;
+                if (boi.boi.Left < 0)
+                {
+                    boi.boi.X = 0;
+                    boi.angle = rng.Next(0, 181);
+                }
+                else if (boi.boi.Right > PB_main.Width)
+                {
+                    boi.boi.X = PB_main.Width - BOISIZE;
+                    boi.angle = rng.Next(180, 360);
+                }
+                if (boi.boi.Top < 0)
+                {
+                    boi.boi.Y = 0;
+                    boi.angle = rng.Next(90, 271);
+                }
+                else if (boi.boi.Bottom > PB_main.Height)
+                {
+                    boi.boi.Y = PB_main.Height - BOISIZE;
+                    boi.angle = rng.Next(0, 1);
+                    if (boi.angle == 0)
+                    {
+                        boi.angle = rng.Next(0, 91);
+                    }
+                    else
+                    {
+                        boi.angle = rng.Next(270, 360);
+                    }
+                }
+            }
+            PB_main.Refresh();
+        }
+
+        private void TB_zoom_Scroll(object sender, EventArgs e)
+        {
+            BOISIZE = TB_zoom.Value;
+            MAG = TB_zoom.Value;
+            Init();
+        }
+        private void TB_turn_Scroll(object sender, EventArgs e)
+        {
+            TURNSPEED = TB_turn.Value;
+            Init();
+        }
+        private void TB_no_Scroll(object sender, EventArgs e)
+        {
+            NUMBER = TB_no.Value;
+            Init();
+        }
+
+        private void Form1_ResizeEnd(object sender, EventArgs e)
+        {
+            PB_main.Height = Height - PNL_constEditor.Height - 50;
+        }
+
+        private void TB_trail_Scroll(object sender, EventArgs e)
+        {
+            TRAILLENGTH = TB_trail.Value;
+            Init();
+        }
+
+        private Point GetXY(double angle, double magnitude)
+        {
+            int x = Convert.ToInt32(magnitude * Math.Sin(GetRads(angle)));
+            int y = Convert.ToInt32(magnitude * Math.Cos(GetRads(angle)));
+            return new Point(x, y);
+        }
+
+        private double GetRads(double angle)
+        {
+            return angle * RADIAN;
+        }
+
+
+
+        private Rectangle Clone(Rectangle input)
+        {
+            return new Rectangle(input.X, input.Y, input.Width, input.Height);
+        }
+    }
+    public class Boi
+    {
+        public Rectangle[] trail;
+        public Rectangle boi;
+        public int angle { get; set; }
+        public Boi(int x, int y, Size size, int trailLength, int angleIn)
+        {
+            trail = new Rectangle[trailLength];
+            boi = new Rectangle(x, y, size.Width, size.Height);
+            angle = angleIn;
+        }
+        public void ShiftBack()
+        {
+            Rectangle[] output = new Rectangle[trail.Length];
+            for (int i = 0; i <= trail.Length - 2; i++)
+            {
+                output[i] = trail[i + 1];
+            }
+            output[trail.Length - 1] = boi;
+            trail = output;
+        }
+    }
+}
